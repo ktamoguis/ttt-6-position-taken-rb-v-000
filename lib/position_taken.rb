e144b85871ff3204472ee774d890962b046cc757
Karl Amoguis <ktamoguis@gmail.com>
2018-04-19 01:19:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-
+  index_new = index.to_i - 1
+  if board[index_new] == " "
+    false
+  elsif board[index_new] == ""
+    false
+  end
 end
